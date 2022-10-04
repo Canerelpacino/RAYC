2b2125609af8a071b214450ffdc2a14a46367dbf
Caner Celik <Caner.Celik@schiffl.de>
2022-10-04 10:13:45 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,354 @@
+import React, { useEffect, useState, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { connect, isconnected } from "./redux/blockchain/blockchainActions";
+import { fetchData } from "./redux/data/dataActions";
+import * as s from "./styles/globalStyles";
+import styled from "styled-components";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+
+const truncate = (input, len) =>
+  input.length > len ? `${input.substring(0, len)}...` : input;
+
+function App() {
+
+  const dispatch = useDispatch();
+  const blockchain = useSelector((state) => state.blockchain);
+  const data = useSelector((state) => state.data);
+  const [claimingNft, setClaimingNft] = useState(false);
+  const [feedback, setFeedback] = useState(``);
+  const [mintAmount, setMintAmount] = useState(1);
+  const [CONFIG, SET_CONFIG] = useState({
+    CONTRACT_ADDRESS: "",
+    SCAN_LINK: "",
+    NETWORK: {
+      NAME: "",
+      SYMBOL: "",
+      ID: 0,
+    },
+    NFT_NAME: "",
+    SYMBOL: "",
+    MAX_SUPPLY: 1,
+    WEI_COST: 0,
+    DISPLAY_COST: 0,
+    GAS_LIMIT: 0,
+    SHOW_BACKGROUND: false,
+  });
+
+  const claimNFTs = () => {
+    let cost = CONFIG.WEI_COST;
+
+    let gasLimit = CONFIG.GAS_LIMIT;
+    let totalCostWei = String(cost * mintAmount);
+    let totalGasLimit = String(gasLimit);
+    console.log("Cost: ", totalCostWei);
+    console.log("Gas limit: ", totalGasLimit);
+    setFeedback(`Have some patience...`);
+    setClaimingNft(true);
+    blockchain.smartContract.methods
+      .mint(mintAmount)
+      .send({
+        gasLimit: String(totalGasLimit),
+        to: CONFIG.CONTRACT_ADDRESS,
+        from: blockchain.account,
+        value: totalCostWei,
+      }) 
+      .once("error", (err) => {
+        console.log(err);
+        setFeedback("Sorry, something went wrong.");
+        setClaimingNft(false);
+      })
+      .then((receipt) => {
+        console.log(receipt);
+        setFeedback(
+          `You got it!!!`
+        );
+        setClaimingNft(false);
+        dispatch(fetchData(blockchain.account));
+      });
+  };
+
+  const decrementMintAmount = () => {
+    let newMintAmount = mintAmount - 1;
+    if (newMintAmount < 1) {
+      newMintAmount = 1;
+    }
+    setMintAmount(newMintAmount);
+  };
+
+  const incrementMintAmount = () => {
+    let newMintAmount = mintAmount + 1;
+    if (newMintAmount > 1) {
+      newMintAmount = 1;
+    }
+    setMintAmount(newMintAmount);
+  };
+
+
+  const getData = () => {
+    if (blockchain.account !== "" && blockchain.smartContract !== null) {
+      dispatch(fetchData(blockchain.account));
+    }
+  };
+
+  const getConfig = async () => {
+    const configResponse = await fetch("/config/config.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    const config = await configResponse.json();
+    SET_CONFIG(config);
+  };
+
+  useEffect(() => {
+    getConfig();
+  }, []);
+
+  useEffect(() => {
+    getData();
+  }, [blockchain.account]);
+
+  const connected = () => {
+    document.getElementById("connectbtn").style.display = "none";
+    document.getElementById("connectbtn2").style.display = "none";
+  };
+
+  return (
+    <div>
+      <div className="home">
+
+      <img src="/config/images/bg.jpg" style={{ width: '100%', height: '100vh'}}></img>
+
+        {/*Socials*/}
+        <a href="https://twitter.com/NotRenga " target="_blank">
+          <img src="/config/images/twitter.png" style={{ width: '7.1%', position: 'absolute', top: '0.6vh', left: '2vw', zIndex: '10' }} className="tw"></img>
+        </a>
+        <a href="https://opensea.io/collection/not-renga" target="_blank">
+          <img src="/config/images/os.png" style={{ width: '7%', position: 'absolute', top: '1vh', right: '100vw', zIndex: '10' }} className="os"></img>
+        </a>
+        <div id="connectbtn" style={{}}
+          onClick={(e) => {
+            e.preventDefault();
+            dispatch(connect());
+            getData();
+            connected();
+          }}
+        >
+        CONNECT
+        </div>
+
+
+        {/*Mint Section*/}
+        <div className="mint">
+          {Number(data.totalSupply) >= CONFIG.MAX_SUPPLY ? (
+            <>
+              <div
+                className="soldout" style={{ fontFamily:  "'Dangrek', cursive", color: 'white', fontSize: '5em', textShadow: '5px 5px black'}}
+              >
+                SOLD OUT!
+              </div>
+              <s.SpacerSmall />
+            </>
+          ) : (
+            <>
+              <s.SpacerXSmall />
+              <s.SpacerSmall />
+              {blockchain.account === "" ||
+                blockchain.smartContract === null ? (
+                <s.Container ai={"center"} jc={"center"}>
+                  <s.SpacerSmall />
+
+                  {blockchain.errorMsg !== "" ? (
+                    <>
+                      <s.SpacerSmall />
+                    </>
+                  ) : null}
+                </s.Container>
+              ) : (
+                <>
+                  <div onLoad={connected()}></div>
+                  <s.SpacerMedium />
+                  <s.Container ai={"center"} jc={"center"} fd={"row"}>
+                    <btn id="roundbtn" className="round-button"
+                      style={{  fontFamily:  "'Dangrek', cursive", color: 'white', fontSize: '5em', cursor: 'pointer', textShadow: '5px 5px black'}}
+                      disabled={claimingNft ? 1 : 0}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        decrementMintAmount();
+                      }}
+                    >
+                      -
+                    </btn>
+                    <s.SpacerMedium />
+                    <s.TextDescription id="mint-amount"
+                      style={{
+                        fontSize: '5em',
+                        textAlign: "center",
+                        color: 'white',  fontFamily:  "'Dangrek', cursive", textShadow: '5px 5px black'
+                      }}
+                    >
+                      {mintAmount}
+                    </s.TextDescription>
+                    <s.SpacerMedium />
+                    <btn className="round-button"
+                      style={{  fontFamily:  "'Dangrek', cursive", color: 'white', fontSize: '5em', cursor: 'pointer', textShadow: '5px 5px black'}}
+                      disabled={claimingNft ? 1 : 0}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        incrementMintAmount();
+                      }}
+                    >
+                      +
+                    </btn>
+                  </s.Container>
+                  <s.Container ai={"center"} jc={"center"} fd={"row"}>
+                    <img className="mintbtn" src="/config/images/mint.png" style={{width: '120%'}}
+                      disabled={claimingNft ? 1 : 0}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        claimNFTs();
+                        getData();
+                      }}
+                    >
+                    </img>
+                  </s.Container>
+                </>
+              )}
+            </>
+          )}
+          </div>
+      </div>
+
+
+      <Phone>
+      <div id="connectbtn2" style={{}}
+          onClick={(e) => {
+            e.preventDefault();
+            dispatch(connect());
+            getData();
+            connected();
+          }}
+        >
+        CONNECT
+        </div>
+
+
+        {/*Mint Section*/}
+        <div className="mint2">
+          {Number(data.totalSupply) >= CONFIG.MAX_SUPPLY ? (
+            <>
+              <div
+                className="soldout" style={{ fontFamily:  "'Dangrek', cursive", color: 'white', fontSize: '5em'}}
+              >
+                SOLD OUT!
+              </div>
+              <s.SpacerSmall />
+            </>
+          ) : (
+            <>
+              <s.SpacerXSmall />
+              <s.SpacerSmall />
+              {blockchain.account === "" ||
+                blockchain.smartContract === null ? (
+                <s.Container ai={"center"} jc={"center"}>
+                  <s.SpacerSmall />
+
+                  {blockchain.errorMsg !== "" ? (
+                    <>
+                      <s.SpacerSmall />
+                    </>
+                  ) : null}
+                </s.Container>
+              ) : (
+                <>
+                  <div onLoad={connected()}></div>
+                  <s.SpacerMedium />
+                  <s.Container ai={"center"} jc={"center"} fd={"row"}>
+                    <btn id="roundbtn" className="round-button"
+                      style={{  fontFamily:  "'Dangrek', cursive", color: 'white', fontSize: '5em', cursor: 'pointer'}}
+                      disabled={claimingNft ? 1 : 0}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        decrementMintAmount();
+                      }}
+                    >
+                      -
+                    </btn>
+                    <s.SpacerMedium />
+                    <s.TextDescription id="mint-amount"
+                      style={{
+                        fontSize: '5em',
+                        textAlign: "center",
+                        color: 'white',  fontFamily:  "'Dangrek', cursive",
+                      }}
+                    >
+                      {mintAmount}
+                    </s.TextDescription>
+                    <s.SpacerMedium />
+                    <btn className="round-button"
+                      style={{  fontFamily:  "'Dangrek', cursive", color: 'white', fontSize: '5em', cursor: 'pointer'}}
+                      disabled={claimingNft ? 1 : 0}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        incrementMintAmount();
+                      }}
+                    >
+                      +
+                    </btn>
+                  </s.Container>
+                  <s.Container ai={"center"} jc={"center"} fd={"row"}>
+                    <div className="mintbtn"
+                      disabled={claimingNft ? 1 : 0}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        claimNFTs();
+                        getData();
+                      }}
+                    >
+                      <p>MINT</p>
+                    </div>
+                  </s.Container>
+                </>
+              )}
+            </>
+          )}
+          </div>
+      </Phone>
+    </div>
+  );
+}
+
+export const Home = styled.div`
+display: flex; 
+flex-direction: column; 
+justify-self: center; 
+align-items: center; 
+height: 100vh; 
+minWidth: 100%;
+background-image: url("/config/images/bg.jpg");
+background-position: 50%; 
+background-repeat: no-repeat;
+background-size: cover; 
+text-align: center; 
+box-sizing: border-box;
+`;
+
+export const Phone = styled.div`
+display: flex; 
+flex-direction: column; 
+justify-self: center; 
+align-items: center; 
+height: 100vh;
+minWidth: 100%;
+background-color: black; 
+text-align: center; 
+@media (orientation: landscape) {
+  display: none;
+}
+`;
+
+export default App;
